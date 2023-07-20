931b03987c62f7de7acf9e966cd479a447b1b2af
Fatih Acıroğlu <aciroglu.fatih@gmail.com>
2023-07-20 17:24:08 +0300

    Initial Commit

---
+++ SwiftUI-Auth-Template/SwiftUI_Auth_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Auth_TemplateApp.swift
+//  SwiftUI-Auth-Template
+//
+//  Created by Fatih Acıroğlu on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Auth_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
